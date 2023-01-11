f7a8e9c102b40655521d86fa7010405040df6007
manuel_durand <manuel.durand@mailo.com>
2023-01-11 20:09:06 +0100

    changement d'adresse de index.php

--- common/header.php
+++ common/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="nav_bar primary">
         <ul>
-            <li><a href="../common/index.php"> Accueil</a></li>
+            <li><a href="../index.php"> Accueil</a></li>
             <li><a href="../content/realisations.php">Réalisations</a></li>
             <li>Veilles Techno.</li>
             <li><a href="../content/cv.php"> C.V.</a></li>
