87bb0a78ef92891633bf72abaea14882f4a2916f
manuel_durand <manuel.durand@mailo.com>
2023-01-11 20:15:36 +0100

    changement de liens index et autres

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include '../common/head.php';
+include './common/head.php';
 ?>
 
 <body>
     <main class="container">
         <?php
-        include 'header.php';
-        include '../content/index_content.php';
-        include 'footer.php';
+        include './common/header.php';
+        include './content/index_content.php';
+        include './common/footer.php';
         ?>
 
     </main>
